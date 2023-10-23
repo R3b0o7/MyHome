841dd65212d286728da0b31d5a0dca66d468f838
Luciano A. Dodaro <dodaroluciano@gmail.com>
2023-10-23 20:24:33 -0300

    Update en.js

---
+++ app/assets/strings/translations/en.js
@@ -0,0 +1,18 @@
+export default {
+    appName: 'Demo1',
+    welcome: 'Bienvenido a React Native',
+    login: 'Logueo',
+    register: 'Registro',
+    recoveryPassword: 'Recupero de contraseña',
+    recoveryPasswordButton: 'Recuperar Contraseña',
+    imageType:{
+        svg:'SVG File',
+        png:'PNG File',
+        jpeg:'JPEG File',
+        webp:'WEBP File',
+        imageFromInternet:'Image donwloaded from internet',
+    },
+    error:{
+    upps : 'Upss algo salió mal',
+    }
+};

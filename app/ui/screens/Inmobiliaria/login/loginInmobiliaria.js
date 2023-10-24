2a44a36b1ae6770d00ab66233ddc1c70c92d9de7
jrenna638 <73914299+jrenna638@users.noreply.github.com>
2023-10-24 15:19:28 -0300

    Update loginInmobiliaria.js

--- app/ui/screens/Inmobiliaria/login/loginInmobiliaria.js
+++ app/ui/screens/Inmobiliaria/login/loginInmobiliaria.js
@@ -33,7 +33,7 @@ const LoginScreen = () => {
                 />
             </View>
             <View style={styles.componentsContainer}>
-                <CustomTextInput label="Usuario o Correo" icon={require('../../../../assets/images/icons/message.png')}/>
+                <CustomTextInput label="Usuario o Correo" icon={require('../../../../assets/images/Icons/message.png')}/>
                 <CustomTextInput placeholder="Contraseña" secureTextEntry={true} />
                 <CustomButton title="Ingresar" color="blue" onPress={handleLogin} style={styles.loginButton} />
                 <CustomButton title="Registrarse" color="green" onPress={handleRegister} />

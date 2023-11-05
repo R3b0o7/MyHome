c0da7856356861322daa9919266ae205efe847a8
Luciano A. Dodaro <100149067+R3b0o7@users.noreply.github.com>
2023-11-05 15:26:54 -0300

    config

--- app/ui/screens/Inmobiliaria/login/LoginLanding.js
+++ app/ui/screens/Inmobiliaria/login/LoginLanding.js
@@ -29,7 +29,7 @@ const LoginLandingScreen = () => {
                 <ImageCustomButton 
                     title={I18n.t('usuario')} 
                     imageSource={require('../../../../assets/images/Icons/lightMode/perfil.png')}
-                    onPress={handleUsuario}
+                    // onPress={handleUsuario}
                     style={styles.buttons} />
                 <ImageCustomButtonBlue 
                     title={I18n.t('inmobiliaria')} 

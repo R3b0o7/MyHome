c035e2cf42c6f14bd40634b1b62db98327a3462a
jrenna638 <73914299+jrenna638@users.noreply.github.com>
2023-11-05 21:12:06 -0300

    recupero de contraseña

--- app/ui/screens/Inmobiliaria/login/RecoveryPassword.js
+++ app/ui/screens/Inmobiliaria/login/RecoveryPassword.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Image } from 'react-native';
 import CustomButton from '../../../components/CustomButton';
 import CustomTextInput from '../../../components/CustomTextInput';
 import I18n from '../../../../assets/strings/I18';
 import { useNavigation } from '@react-navigation/native';
+import { SERVER_URL } from '../../../../config/config';
+import axios from 'axios';
 
 const ReCoveryScreen = () => {
     const navigation = useNavigation();
+    const [email, setEmail] = useState(''); // Estado para almacenar el correo electrónico
 
-    // Función vacía para manejar la acción de registro
-    const handleRescoveryPassword = () => {
-        alert('Se envió un correo a tu mail. Por favor revisalo');
-        navigation.goBack();
+
+    // Función para manejar la acción de recuperación de contraseña
+    const handleRescoveryPassword = async () => {
+        try {
+            
+            if (email.trim() === '') {
+                // Validación simple para asegurarse de que se haya ingresado un correo electrónico
+                alert('Por favor, ingrese su correo electrónico.');
+                return;
+            }
+
+            // Realiza la solicitud al servidor con el correo electrónico ingresado
+            const response = await axios.post(`${SERVER_URL}/api/users/recovery-request`, { email: email });
+            
+            if (response.status === 200) {
+                // Si la solicitud se completó con éxito, muestra un mensaje al usuario
+                alert('Se ha enviado un correo a tu dirección de correo. Por favor, revísalo.');
+                navigation.goBack();
+            } else {
+                // Maneja otros escenarios de respuesta aquí, por ejemplo, si el correo no existe
+                alert('Hubo un problema al enviar el correo de recuperación.');
+            }
+        } catch (error) {
+            // Maneja errores de red u otros errores
+            alert('Error en la solicitud de recuperación de contraseña');
+        }
     };
 
     return (
         <View style={styles.container}>
 
             <View style={styles.componentsContainer}>
-                <CustomTextInput label={I18n.t('mail')} icon={require('../../../../assets/images/Icons/black/email.png')}/>
+            <CustomTextInput
+                    label={I18n.t('mail')}
+                    icon={require('../../../../assets/images/Icons/black/email.png')}
+                    value={email}
+                    onChangeText={(text) => setEmail(text)} // Actualiza el estado del correo electrónico
+                />
                 <CustomButton title={I18n.t('recoveryPasswordButton')} color="green" onPress={handleRescoveryPassword} style={styles.rescoveryPasswordButton} />
             </View>
         </View>
@@ -43,5 +73,6 @@ const styles = StyleSheet.create({
     },
 
 });
+
 
 export default ReCoveryScreen;

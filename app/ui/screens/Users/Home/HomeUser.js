d17e77f9b05b6e4c1a200fb3e66188b7cf763765
jrenna638 <73914299+jrenna638@users.noreply.github.com>
2023-11-17 13:04:17 -0300

    correccion mas cercanas

--- app/ui/screens/Users/Home/HomeUser.js
+++ app/ui/screens/Users/Home/HomeUser.js
@@ -18,55 +18,90 @@ const HomeUser = () => {
 
     const [userProperties, setUserProperties] = useState([]); // Estado para almacenar las propiedades del usuario
 
+    //CALCULADORA DE DISTANCIA
+    const calculateDistance = (lat1, lon1, lat2, lon2) => {
+        console.log(`Calculando distancia entre (${lat1}, ${lon1}) y (${lat2}, ${lon2})`);
+        const R = 6371; // Radio de la Tierra en km
+        const dLat = (lat2 - lat1) * Math.PI / 180;
+        const dLon = (lon2 - lon1) * Math.PI / 180;
+        const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return R * c; // Distancia en km
+    };
+
+    //OBTENER UBICACION
+    const getCurrentLocation = async () => {
+        const hasPermission = await requestLocationPermission();
+        if (!hasPermission) {
+            console.log('Location permission not granted');
+            return;
+        }
+
+        Geolocation.getCurrentPosition(
+            (position) => {
+                const location = {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                };
+
+                console.log(`Ubicación actual: ${JSON.stringify(location)}`); // Convertir a cadena JSON
+                setCurrentLocation(location); // Asegúrate de que esto se ejecute antes de llamar a fetchUserProperties
+                fetchUserProperties(location); // Llama a fetchUserProperties con la ubicación actual
+            },
+            (error) => console.error(error),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+        );
+    };
+
     const fetchUserProperties = async (currentLocation) => {
-
         try {
-            const requestBody = {};
-
-            // Realizar la petición al servidor
+            const requestBody = {}; // Asegúrate de que requestBody sea correcto
             const response = await axios.post(`${SERVER_URL}/api/properties/search`, requestBody);
 
             if (response.status === 200) {
-                // Manejar la respuesta del servidor
                 const propertiesWithDistance = response.data.map(prop => {
-                    if (prop.coordenadas && prop.coordenadas.latitude && prop.coordenadas.longitude) {
-                        const distance = calculateDistance(currentLocation.latitude, currentLocation.longitude, prop.coordenadas.latitude, prop.coordenadas.longitude);
+                    //console.log(`Propiedad: ${prop._id}, Coordenadas: ${JSON.stringify(prop.coordenadas)}`); // Convertir a cadena JSON
+                    if (prop.coordenadas && Array.isArray(prop.coordenadas.coordinates)) {
+                        const [latitude, longitude] = prop.coordenadas.coordinates; // Usa primero la longitud y luego la latitud
+                        const distance = calculateDistance(currentLocation.latitude, currentLocation.longitude, latitude, longitude);
                         return { ...prop, distance };
                     } else {
                         return { ...prop, distance: Number.MAX_VALUE };
                     }
                 });
-    
+
                 const sortedProperties = propertiesWithDistance.sort((a, b) => a.distance - b.distance);
+                //console.log('Propiedades ordenadas:', sortedProperties.map(p => ({ id: p._id, distance: p.distance })));
                 setUserProperties(sortedProperties);
 
                 //MUESTRA LAS COORDENADAS DE LA PROPIEDAD
-                /*userProperties.forEach(propiedad => {
+                sortedProperties.forEach(propiedad => {
                     // Verificar si la propiedad tiene coordenadas
                     if (propiedad.coordenadas && Array.isArray(propiedad.coordenadas.coordinates)) {
                         // Acceder a las coordenadas
                         const coordenadas = propiedad.coordenadas.coordinates;
-                        
+
                         // Longitud y Latitud
                         const longitud = coordenadas[1];
                         const latitud = coordenadas[0];
-                
+
                         // Mostrar coordenadas por consola
-                        console.log(`Propiedad ID: ${propiedad._id}, Latitud: ${latitud}, Longitud: ${longitud}`);
+                        console.log(`Propiedad: ${propiedad.calle} ${propiedad.numero}, Latitud: ${latitud}, Longitud: ${longitud}, distancia: ${propiedad.distance}`);
                     } else {
                         // Manejo de casos donde no hay coordenadas disponibles
                         console.log(`Propiedad ID: ${propiedad._id} no tiene coordenadas disponibles.`);
                     }
-                });*/
-
+                });
             } else {
                 console.error('Respuesta no exitosa:', response);
             }
         } catch (error) {
             console.error('Error en la petición de búsqueda:', error);
         }
-
     };
+
     //PERMISOS
     const requestLocationPermission = async () => {
         if (Platform.OS === 'ios') {
@@ -88,56 +123,13 @@ const HomeUser = () => {
     };
     const [currentLocation, setCurrentLocation] = useState(null);
 
-    //OBTENER UBICAION
-    const getCurrentLocation = async () => {
-
-        const hasPermission = await requestLocationPermission();
-        if (!hasPermission) {
-            console.log('Location permission not granted');
-            return;
-        }
-        Geolocation.getCurrentPosition(
-            (position) => {
-                const location  = {
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude,
-                };
-
-                setCurrentLocation(location);
-                fetchUserProperties(location);
+    //console.log(userProperties);
 
 
-
-                //console.log(currentLocation);
-                // Aquí puedes llamar a una función para obtener y ordenar las propiedades
-            },
-            (error) => console.error(error),
-            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-        );
-    };
-
-    //CALCULADORA DE DISTANCIA
-    const calculateDistance = (lat1, lon1, lat2, lon2) => {
-        const R = 6371; // Radio de la Tierra en km
-        const dLat = (lat2 - lat1) * Math.PI / 180;
-        const dLon = (lon2 - lon1) * Math.PI / 180;
-        const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
-            Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        return R * c; // Distancia en km
-    };
 
     useEffect(() => {
-        const unsubscribe = navigation.addListener('focus', () => {
-            // Este código se ejecutará cada vez que la pantalla esté en primer plano
-            getCurrentLocation();
-            fetchUserProperties();
-        });
-
-        return unsubscribe;
-    }, [navigation]);
-
+        getCurrentLocation();
+    }, []);
 
     const handleCardHorizontalPress = () => {
         // Define aquí la lógica de navegación
@@ -163,15 +155,15 @@ const HomeUser = () => {
 
     const renderItem = ({ item, index }) => {
         return (
-            <View style={styles.slide}>
+            <View style={styles.slide} key={item._id}>
                 <CustomCard
                     key={index}
                     address={item.calle + ' ' + item.numero}
                     description={
                         (item.alquiler ? 'Alquiler' :
-                        item.venta ? 'Venta' :
-                        item.reservada ? 'Reservada' :
-                        item.alquiladaVendida ? 'Alquilada o Vendida' : '')
+                            item.venta ? 'Venta' :
+                                item.reservada ? 'Reservada' :
+                                    item.alquiladaVendida ? 'Alquilada o Vendida' : '')
                         + ' - ' + item.localidad // Aquí puedes añadir tu texto
                     }
                     coverUrl={getRandomImageUrl(item.photos)}

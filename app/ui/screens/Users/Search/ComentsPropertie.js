61805f4ae97e8fc35efd7c1ed025f86bb4d4544c
jrenna638 <73914299+jrenna638@users.noreply.github.com>
2023-11-30 15:54:42 -0300

    act

--- app/ui/screens/Users/Search/ComentsPropertie.js
+++ app/ui/screens/Users/Search/ComentsPropertie.js
@@ -1,28 +1,16 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Text, ScrollView, Image } from 'react-native';
-import {  Paragraph, Modal } from 'react-native-paper';
-
-import { useNavigation } from '@react-navigation/native';
-import NavigatorConstant from '../../../../navigation/NavigatorConstant';
-import axios from 'axios';
-import { SERVER_URL } from '../../../../config/config';
-
+import { Paragraph, Modal } from 'react-native-paper';
 import I18n from '../../../../assets/strings/I18';
+import { useNavigation } from '@react-navigation/native';
 import RatingStars from '../../../components/RatingStars';
 import CustomCommentsCard from '../../../components/CustomCommentsCard';
 
-
 const ComentsPropertie = () => {
-    
-    // El lorem impsum NO VA, es para pasarle un texto largo al componente de prueba
-    const loremImpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id semper risus in hendrerit gravida. Bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim. Ac tortor vitae purus faucibus ornare. Morbi leo urna molestie at elementum eu facilisis sed. Sem nulla pharetra diam sit amet nisl suscipit. Non quam lacus suspendisse faucibus interdum posuere lorem ipsum. Vel turpis nunc eget lorem dolor. Scelerisque felis imperdiet proin fermentum leo vel orci porta. Ultricies tristique nulla aliquet enim tortor at auctor urna nunc. Amet aliquam id diam maecenas ultricies mi eget. Aliquam nulla facilisi cras fermentum odio eu feugiat pretium nibh. Eget magna fermentum iaculis eu. Neque convallis a cras semper."
+    const loremImpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ..."; // Texto largo de ejemplo
 
     const navigation = useNavigation();
-
-    // MODAL
     const [visible, setVisible] = useState(false);
-
-    // Definir las propiedades por defecto para pasarle despues al modal las seleccionadas
     const [selectedComment, setSelectedComment] = useState({
         userName: '',
         commentText: '',
@@ -30,44 +18,61 @@ const ComentsPropertie = () => {
         rating: 0,
     });
 
-    // Función para mostrar el modal con la información del comentario seleccionado pasados por parametro
+    // Comentarios de ejemplo (podrían venir de una API)
+    const [comments, setComments] = useState([
+        {
+            userName: "Jhone Doe",
+            userPhoto: 'https://picsum.photos/701',
+            commentText: loremImpsum,
+            rating: 4,
+        },
+        {
+            userName: "Pepe",
+            userPhoto: 'https://picsum.photos/702',
+            commentText: loremImpsum,
+            rating: 2,
+        },
+        // Agrega más comentarios aquí si es necesario
+    ]);
+
     const showModal = (comment) => {
         setSelectedComment(comment);
         setVisible(true);
-      };
+    };
 
-    // Función para ocultar el modal, vuelve a estableser el estado de las propiedades vacias
     const hideModal = () => {
-        setSelectedComment({ userName: '', commentText: '', userPhoto: null, rating: 0 });
         setVisible(false);
-      };
+    };
 
     return (
-        <View  style={styles.PrincipalContainer}>
+        <View style={styles.PrincipalContainer}>
             <ScrollView>
                 <View style={styles.TitleConteiner}>
-                    <Image style={styles.ImageTitle} source={require('../../../../assets/images/Icons/lightMode/message.png')}/>
+                    <Image style={styles.ImageTitle} source={require('../../../../assets/images/Icons/lightMode/message.png')} />
                     <Text style={styles.Title}>Comentarios</Text>
                 </View>
-                <CustomCommentsCard
-                    userName="Jhone Doe"
-                    userPhoto= 'https://picsum.photos/701'
-                    commentText= {loremImpsum}
-                    rating={4}
-                    onSelectComment={showModal}
-                />
+                {comments.map((comment, index) => (
+                    <CustomCommentsCard
+                        key={index}
+                        userName={comment.userName}
+                        userPhoto={comment.userPhoto}
+                        commentText={comment.commentText}
+                        rating={comment.rating}
+                        onSelectComment={() => showModal(comment)}
+                    />
+                ))}
             </ScrollView>
 
             <Modal
-            visible={visible}
-            onDismiss={hideModal}
-            contentContainerStyle={[styles.modalContainer, styles.modalContent]}
-            dismissable={true} // Permite cerrar el modal al tocar fuera de él
+                visible={visible}
+                onDismiss={hideModal}
+                contentContainerStyle={[styles.modalContainer, styles.modalContent]}
+                dismissable={true}
             >
                 <View>
                     <View style={styles.topRow}>
                         {selectedComment.userPhoto !== null ? (
-                        <Image style={styles.userImageStyle} source={{ uri: selectedComment.userPhoto }}/>
+                            <Image style={styles.userImageStyle} source={{ uri: selectedComment.userPhoto }} />
                         ) : null}
                         <Text style={styles.userNameStyle}>{selectedComment.userName}</Text>
                     </View>
@@ -79,15 +84,12 @@ const ComentsPropertie = () => {
                     </View>
                 </View>
             </Modal>
-
         </View>
-        
     );
 };
 
 const styles = StyleSheet.create({
-    // CONTENEDORES
-    PrincipalContainer:{
+    PrincipalContainer: {
         flex: 1,
         alignItems: 'center'
     },
@@ -96,7 +98,6 @@ const styles = StyleSheet.create({
         marginTop: 10,
         padding: 10
     },
-    // ENCABEZADO
     ImageTitle: {
         width: 35,
         height: 35,
@@ -107,7 +108,6 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color: 'black'
     },
-    // MODAL
     modalContainer: {
         backgroundColor: 'white',
         padding: 20,
@@ -139,7 +139,7 @@ const styles = StyleSheet.create({
     starsContainer: {
         alignItems: 'flex-end',
         marginTop: 5,
-      },
+    },
 });
 
 export default ComentsPropertie;

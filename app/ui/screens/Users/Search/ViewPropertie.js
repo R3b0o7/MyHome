3ba912af66bf2a0fb1711e11f5246014421aeca7
Luciano A. Dodaro <100149067+R3b0o7@users.noreply.github.com>
2023-11-30 16:31:25 -0300

    comentarios carga

--- app/ui/screens/Users/Search/ViewPropertie.js
+++ app/ui/screens/Users/Search/ViewPropertie.js
@@ -43,7 +43,6 @@ const ViewPropertie = ({ route }) => {
         }
     }, [isFocused]);
 
-
     const handleReserv = () => {
         navigation.push(NavigatorConstant.SEARCH_.RESERVE_PROPERTIES, {
             propertyId: route.params.propertyId

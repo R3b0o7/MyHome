26c2a05c3e434081fa7a7c7fc2e2e3da914e2dd2
Luciano A. Dodaro <100149067+R3b0o7@users.noreply.github.com>
2023-12-03 17:24:07 -0300

    error corection

--- app/ui/screens/Inmobiliaria/TabNavigator/PropertiesUpdate.js
+++ app/ui/screens/Inmobiliaria/TabNavigator/PropertiesUpdate.js
@@ -1108,7 +1108,7 @@ const styles = StyleSheet.create({
         width: 300,
         backgroundColor: '#E0E4F2',
         borderColor: '#E0E4F2',
-
+    },
     uploadphotoButton: {
         alignSelf: 'center',
         width: 150,
@@ -1188,7 +1188,6 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
     },
-
 });
 
 export default PropertiesUpdate;
